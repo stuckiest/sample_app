3a4b7db3d81165b11bf014531537cbfebc30de57
stuckiest <stuckiest@gmail.com>
2016-04-19 16:23:19 -0600

    add contact and improve views

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_response :success
   	assert_select "title", "About | Ruby on Rails"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails"
+  end
 end
