924cd976de650b648a3756f378ef96c9c834a9fb
stuckiest <stuckiest@gmail.com>
2016-04-28 20:13:59 -0600

    add sessions controller and login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
 
@@ -10,6 +9,12 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'signup' => 'users#new'
+
+  get    'login'   => 'sessions#new'
+  
+  post   'login'   => 'sessions#create'
+  
+  delete 'logout'  => 'sessions#destroy' 
 
   resources :users
 
